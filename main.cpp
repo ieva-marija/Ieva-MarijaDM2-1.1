137da738cab202cbbf7cb370692e357f07508d54
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 20:24:47 +0300

    Update main.cpp
    issk

--- main.cpp
+++ main.cpp
@@ -1,49 +1,18 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
-#include <vector>
+#include "mylib.h"
 
-using std::string;
-using std::vector;
+using namespace std;
 
-Studentas ivesk();
-
-struct Studentas {
-    std::string vardas, pavarde;
-    std::vector<int> pazymiai;
-    int egzaminas;
-    float vidurkis, mediana, rezultatas;
-};
-
-Studentas ivesk()
+int main()
 {
-  Studentas temp;
-  cout << "Iveskite varda: ";
-  cin >> temp.vardas;
-  cout << "Iveskite pavarde: ";
-  cin >> temp.pavarde;
-  cout << "Iveskite pazymiu kieki: ";
-    int n;
-    cin >> n;
-    temp.vidurkis = 0;
-    for (int i = 0; i < n; i++) {
-        int pazymys;
-        cout << "Iveskite pazymi: ";
-        cin >> pazymys;
-        temp.pazymiai.push_back(pazymys);
-        temp.vidurkis += pazymys;
-    }
-    if (n > 0) {
-        temp.vidurkis = static_cast<float>(temp.vidurkis) / n;
-    }
-  cout << "Iveskite egzamino pazymi: ";
-  cin >> temp.egzaminas;
-  temp.rezultatas=0.4*temp.vidurkis+0.6*temp.egzaminas;
-  return temp;
+    Studentas A;
+    A = ivesk();
+ 
+    cout << "Studento duomenys:" << endl;
+    cout << "Vardas: " << A.vardas 
+    cout << "Pavarde: " << A.pavarde << endl;
+    cout << "Galutinis balas: " << fixed << setprecision(2) << A.rezultatas << endl;
+
+    return 0;
 }
-
-
-
-
-
-
