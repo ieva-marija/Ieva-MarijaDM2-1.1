c927543fff0d45206bc489f8b493711b27c253bb
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 19:41:43 +0300

    Update main.cpp
    ivedami duomenys - pazymiu vektorius

--- main.cpp
+++ main.cpp
@@ -6,9 +6,27 @@
 using std::string;
 using std::vector;
 
+Studentas ivesk();
+
 struct Studentas {
     std::string vardas, pavarde;
     std::vector<int> pazymiai;
     int egzaminas;
     float vidurkis, mediana, rezultatas;
 };
+
+Studentas ivesk()
+{
+  Studentas temp;
+  cout << "Iveskite varda: ";
+  cin >> temp.vardas;
+  cout << "Iveskite pavarde: ";
+  cin >> temp.pavarde;
+  cout << "Iveskite pazymiu kieki: ";
+  int n;
+  cin >> n;
+    //ciklas
+  cout << "Iveskite egzamino pazymi: ";
+  cin >> temp.egzaminas;
+  return temp;
+}
