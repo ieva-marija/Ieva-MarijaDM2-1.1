5849e74b4d5d3b160842bab1394edfa2fb109b11
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 20:35:23 +0300

    Update mylib.cpp

--- mylib.cpp
+++ mylib.cpp
@@ -1,26 +1,29 @@
 #include "mylib.h"
+#include <algorithm>
 
 Studentas ivesk()
 {
   Studentas temp;
-  cout << "Iveskite varda: ";
+  cout << "Įveskite vardą: ";
   cin >> temp.vardas;
-  cout << "Iveskite pavarde: ";
+  cout << "Įveskite pavardę: ";
   cin >> temp.pavarde;
-  cout << "Iveskite pazymiu kieki: ";
-    int n;
-    cin >> n;
-    temp.vidurkis = 0;
-    for (int i = 0; i < n; i++) {
-        int pazymys;
-        cout << "Iveskite pazymi: ";
-        cin >> pazymys;
-        temp.pazymiai.push_back(pazymys);
-        temp.vidurkis += pazymys;
+  cout << "Įveskite pažymių kiekį: ";
+  int n;
+  cin >> n;
+  temp.vidurkis=0;
+  for(int i=0; i<n; i++)
+    {
+      int pazymys;
+      cout << "Iveskite pazymi: ";
+      cin >> pazymys;
+      temp.pazymiai.push_back(pazymys);
+      temp.vidurkis+=pazymys;
     }
-    if (n > 0) {
-        temp.vidurkis = static_cast<float>(temp.vidurkis) / n;
-    }
+  if (n>0)
+  {
+      temp.vidurkis=static_cast<float>(temp.vidurkis/n);
+  }
   sort(temp.pazymiai.begin(), temp.pazymiai.end());
     if (n % 2 == 0) {
         int vidurys = n / 2;
@@ -28,8 +31,17 @@ Studentas ivesk()
     } else {
         temp.mediana = static_cast<float>(temp.pazymiai[n / 2]);
     }
-  cout << "Iveskite egzamino pazymi: ";
+  cout << "Įveskite egzamino pažymį: ";
   cin >> temp.egzaminas;
-  
   return temp;
-}
+};
+
+float galutinisVid(const Studentas& studentas) {
+    float vidurkis = studentas.vidurkis;
+    return 0.4 * vidurkis + 0.6 * studentas.egzaminas;
+};
+
+float galutinisMed(const Studentas& studentas) {
+    float mediana = studentas.mediana;
+    return 0.4 * mediana + 0.6 * studentas.egzaminas;
+};
