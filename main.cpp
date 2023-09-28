cb5ec9253a3eafa2850644a67430706c263afebd
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 19:34:54 +0300

    Create main.cpp
    struktura, skirta duomenims saugoti

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct Studentas {
+    std::string vardas, pavarde;
+    std::vector<int> pazymiai;
+    int egzaminas;
+    float vidurkis, mediana, rezultatas;
+};
