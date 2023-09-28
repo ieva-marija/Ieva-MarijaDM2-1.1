4ceda99598956415fe8c9de4ee22dcf13c921972
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 20:23:33 +0300

    Create mylib.h
    issk

---
+++ mylib.h
@@ -0,0 +1,31 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cstdio>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::left;
+using std::right;
+
+
+struct Studentas {
+    std::string vardas, pavarde;
+    std::vector<int> pazymiai;
+    int egzaminas;
+    float vidurkis, mediana, rezultatas;
+};
+
+Studentas ivesk();
+
+#endif // MYLIB_H
