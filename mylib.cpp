d725e14f82ac249eb44dd96936e1ae21b957d094
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-28 20:30:52 +0300

    Update mylib.cpp

--- mylib.cpp
+++ mylib.cpp
@@ -21,8 +21,15 @@ Studentas ivesk()
     if (n > 0) {
         temp.vidurkis = static_cast<float>(temp.vidurkis) / n;
     }
+  sort(temp.pazymiai.begin(), temp.pazymiai.end());
+    if (n % 2 == 0) {
+        int vidurys = n / 2;
+        temp.mediana = static_cast<float>(temp.pazymiai[vidurys - 1] + temp.pazymiai[vidurys]) / 2;
+    } else {
+        temp.mediana = static_cast<float>(temp.pazymiai[n / 2]);
+    }
   cout << "Iveskite egzamino pazymi: ";
   cin >> temp.egzaminas;
-  temp.rezultatas=0.4*temp.vidurkis+0.6*temp.egzaminas;
+  
   return temp;
 }
