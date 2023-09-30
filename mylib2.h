799c103688f809d9ba577595f72965707293dc83
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-09-30 20:06:52 +0300

    Create mylib2.h

---
+++ mylib2.h
@@ -0,0 +1,30 @@
+#ifndef MYLIB2_H_INCLUDED
+#define MYLIB2_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::left;
+using std::right;
+using std::sort;
+
+Studentas ivesk();
+int rankinisFailinis(vector<Studentas>& studentai);
+void skaityti(vector<Studentas>& studentai, const string& pav);
+float galutinisVid(const Studentas& studentas);
+float galutinisMed(const Studentas& studentas);
+void rusiavimas(vector<Studentas>& studentai);
+
+#endif // MYLIB2_H_INCLUDED
