d49da1aa28d087d7ee47625ec8e5e588dc10c398
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-10-29 18:53:34 +0200

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -5,13 +5,22 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <list>
 
 using std::string;
 using std::vector;
+using std::list;
 
 struct Studentas {
     std::string vardas, pavarde;
     std::vector<int> pazymiai;
+    int egzaminas;
+    float vidurkis, mediana, rezultatasv, rezultatasm;
+};
+
+struct Studentas2 {
+    std::string vardas, pavarde;
+    std::list<int> pazymiai;
     int egzaminas;
     float vidurkis, mediana, rezultatasv, rezultatasm;
 };
