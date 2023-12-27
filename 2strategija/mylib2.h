04f2ca5febbf53587e1854276e22e350650f503d
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-12-27 16:10:35 +0200

    Update mylib2.h

--- 2strategija/mylib2.h
+++ 2strategija/mylib2.h
@@ -30,40 +30,46 @@ float galutinisMedList(const Studentas2& studentas);
 
 void skaityti(vector<Studentas>& studentai, const string& pav);
 void rusiavimasVector(vector<Studentas>& studentai);
+void rusiavimasVector2(vector<Studentas>& studentai);
+void rusiavimasVector3(vector<Studentas>& studentai);
 void rusiavimasList(list<Studentas2>& studentai);
+void rusiavimasList2(list<Studentas2>& studentai2);
+void rusiavimasList3(list<Studentas2>& studentai2);
 int rankinisFailinis(vector<Studentas>& studentai);
 
-void generavimasVector(vector<Studentas>& studentai, int count, const string& failas);
+void generavimasVector(vector<Studentas>& studentai, int count, const string& failas, const string& failas0, int skaicius);
+void saugojimas0Vector(const string& failas0, const vector<Studentas>& pradinis);
 void saugojimasVector(const string& failas, const vector<Studentas>& vargsiukai);
 void skaityti2Vector(vector<Studentas>& studentai, const string& pav);
 
-void rusiavimas2Vector(vector<Studentas>& studentai, vector<Studentas>& vargsiukai);
+void rusiavimas2Vector(const vector<Studentas>& studentai, vector<Studentas>& vargsiukai, int skaiciux);
 void isvedimasVVector(const string& failas_vargsiukai, const vector<Studentas>& vargsiukai);
 void isvedimasKVector(const string& failas_kietiakai, const vector<Studentas>& studentai);
 
-void generavimoLVector(vector<Studentas>& studentai, int skaicius, const string& failas);
-void skaitymoLVector(vector<Studentas>& studentai, int skaicius, const string& failas);
-void rusiavimoLVector(vector<Studentas>& studentai, int skaicius, vector<Studentas>& vargsiukai);
-void isvedimoVLVector(const string& failas_vargsiukai, int skaicius, const vector<Studentas>& vargsiukai);
-void isvedimoKLVector(const string& failas_kietiakai, int skaicius, const vector<Studentas>& studentai);
+void generavimoLVector(vector<Studentas>& studentai, int skaicius, const string& failas, const string& failas0, int sk);
+void skaitymoLVector(vector<Studentas>& studentai, const string& failas);
+void rusiavimoLVector(vector<Studentas>& studentai, vector<Studentas>& vargsiukai, int skaiciux);
+void isvedimoVLVector(const string& failas_vargsiukai, const vector<Studentas>& vargsiukai);
+void isvedimoKLVector(const string& failas_kietiakai, const vector<Studentas>& studentai);
 
-void testavimasv(const string& failoPav, int skaicius, const string& kietiakaiF, const string& vargsiukaiF);
+void testavimasv(const string& failoPav, const string& kietiakaiF, const string& vargsiukaiF, int skaiciux);
 
 //list
 
 void generavimasList(list<Studentas2>& studentai2, int count, const string& failas);
+void saugojimas0List(const string& failas0, const list<Studentas2>& pradinis);
 void saugojimasList(const string& failas, const list<Studentas2>& studentai2);
 void skaityti2List(list<Studentas2>& studentai2, const string& pav);
 
-void rusiavimas2List(list<Studentas2>& studentai2, list<Studentas2>& vargsiukai);
+void rusiavimas2List(const list<Studentas2>& studentai2, list<Studentas2>& vargsiukai, int skaiciux);
 void isvedimasVList(const string& failas_vargsiukai, const list<Studentas2>& vargsiukai);
 void isvedimasKList(const string& failas_kietiakai, const list<Studentas2>& studentai2);
 
-void generavimoLList(list<Studentas2>& studentai2, int skaicius, const string& failas);
-void skaitymoLList(list<Studentas2>& studentai2, int skaicius, const string& failas);
-void rusiavimoLList(list<Studentas2>& studentai2, int skaicius, list<Studentas2>& vargsiukai);
-void isvedimoVLList(const string& failas_vargsiukai, int skaicius, const list<Studentas2>& vargsiukai);
-void isvedimoKLList(const string& failas_kietiakai, int skaicius, const list<Studentas2>& studentai2);
+void generavimoLList(list<Studentas2>& studentai2, int skaicius, const string& failas, const string& failas0, int sk);
+void skaitymoLList(list<Studentas2>& studentai2, const string& failas);
+void rusiavimoLList(list<Studentas2>& studentai2, list<Studentas2>& vargsiukai, int skaiciux);
+void isvedimoVLList(const string& failas_vargsiukai, const list<Studentas2>& vargsiukai);
+void isvedimoKLList(const string& failas_kietiakai, const list<Studentas2>& studentai2);
 
-void testavimasl(const string& failoPav, int skaicius, const string& kietiakaiF, const string& vargsiukaiF);
+void testavimasl(const string& failoPav, const string& vargsiukaiF, const string& kietiakaiF, int skaiciux);
 #endif
