a55b19bb7fdb29b5e7e0bce8e9d3d780a72a6c1d
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-12-27 16:36:32 +0200

    Update mylib.cpp

--- mylib.cpp
+++ mylib.cpp
@@ -76,6 +76,73 @@ Studentas ivesk()
      temp.rezultatasm=galutinisMedVector(temp);
   return temp;
 };
+//---------------------------------------------------------------------
+Studentas2 ivesk2()
+{
+  Studentas2 temp;
+  cout << "Iveskite varda: ";
+  cin >> temp.vardas;
+  cout << "Iveskite pavarde: ";
+  cin >> temp.pavarde;
+
+  int pazymys;
+  int pasirinkimas2;
+  cout << "Ar norite, kad programa studento pazymius ir egzamino rezultata generuotu atsitiktinai?" << endl;
+  cout << "5 - taip, noriu, aciu" << endl;
+  cout << "9 - ne, noriu ivesti savarankiskai" << endl;
+  cin >> pasirinkimas2;
+
+    if (pasirinkimas2 == 5) {
+        srand(time(0));
+        int a_pazymiai_sk = rand() % 11;
+        cout << "Sugeneruoti atsitiktinai pazymiai: ";
+        for (int i = 0; i < a_pazymiai_sk; ++i) {
+            int a_pazymys = rand() % 11 + 1;
+            temp.pazymiai.push_back(a_pazymys);
+            cout << a_pazymys << " ";
+        }
+        srand(time(0));
+        temp.egzaminas = rand() % 11 + 1;
+        cout << "Sugeneruotas egzamino pazymys: "<< temp.egzaminas << endl;
+        cout << "\n";
+    }
+     else if (pasirinkimas2 == 9) {
+       while (true) {
+            cout << "Iveskite namu darbu rezultatus (iveskite -1, kai suvedete visus norimus pazymius): ";
+            if (cin >> pazymys) {
+                if (pazymys == -1) {
+                    cin.clear();
+                    cin.ignore(256, '\n');
+                    break;
+                }
+                if (pazymys >= 0 && pazymys <= 10) {
+                    temp.pazymiai.push_back(pazymys);
+                }
+                else {
+                    cout << "??????????" << endl;
+                    }
+            }
+            else {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "????????????" << endl;
+                }
+       }
+       cout << "Iveskite egzamino pazymi: ";
+        cin >> temp.egzaminas;
+        while (temp.egzaminas > 10) {
+            cout << "MELAGIS. VESK IS NAUJO";
+            cin >> temp.egzaminas;
+        }
+     }
+     else {
+        cout << "nu cia nei 5, nei 9........" << endl;
+        return temp;
+     }
+     temp.rezultatasv=galutinisVidList(temp);
+     temp.rezultatasm=galutinisMedList(temp);
+  return temp;
+};
 //----------------------------------------------------------------
 int rankinisFailinis(vector<Studentas>& studentai) {
 
