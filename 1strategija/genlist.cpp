fa5f7b709413d6597f56e9980136f554568787f8
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-12-27 16:17:26 +0200

    Update genlist.cpp

--- 1strategija/genlist.cpp
+++ 1strategija/genlist.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void generavimasList(list<Studentas2>& studentai2, int count, const string& failas)
+void generavimasList(list<Studentas2>& studentai2, int count, const string& failas, const string& failas0, int skaicius)
 {
     studentai2.clear();
     for (int i = 1; i < count +1; i++) {
@@ -24,10 +24,46 @@ void generavimasList(list<Studentas2>& studentai2, int count, const string& fail
         studentas.rezultatasm = galutinisMedList(studentas);
         studentai2.push_back(studentas);
     }
-    rusiavimasList(studentai2);
+    list<Studentas2> pradinis = studentai2;
+    saugojimas0List(failas0, pradinis);
+    if(skaicius==0){
+        rusiavimasList2(studentai2);
+    }
+    else if(skaicius==1){
+        rusiavimasList(studentai2);
+    }
+    else if (skaicius==2){
+        rusiavimasList3(studentai2);
+    }
     saugojimasList(failas, studentai2);
 }
-
+//--------------------------------------------------------------------------------------
+void saugojimas0List(const string& failas0, const list<Studentas2>& pradinis)
+{
+    ofstream F(failas0);
+    if (!F) {
+        cerr << "Nepavyko atidaryti failo " << failas0 << endl;
+        return;
+    }
+    int maxNumND = 0;
+    for (const Studentas2& studentas : pradinis) {
+        maxNumND = max(maxNumND, static_cast<int>(studentas.pazymiai.size()));
+    }
+    F << left << setw(15) << "Vardas" << setw(17) << " Pavarde";
+    for(int i=0; i<maxNumND; i++){
+        F << setw(10) <<  "Nd." + to_string(i);
+    }
+    F << setw(15) << "Egzaminas " << endl;
+    for (const Studentas2& studentas : pradinis) {
+        F << setw(15) << studentas.vardas << " " << setw(17) << studentas.pavarde;
+        for(auto it = studentas.pazymiai.begin(); it != studentas.pazymiai.end(); ++it){
+            F << setw(10) << *it;
+        }
+        F << studentas.egzaminas <<  endl;
+    }
+    F.close();
+}
+//------------------------------------------------------------------------------
 void saugojimasList(const string& failas, const list<Studentas2>& studentai2)
 {
     ofstream F(failas);
@@ -42,7 +78,7 @@ void saugojimasList(const string& failas, const list<Studentas2>& studentai2)
     }
     F.close();
 }
-
+//-------------------------------------------------------------------------------------
 void skaityti2List(list<Studentas2>& studentai2, const string& pav) {
     ifstream F(pav);
 
@@ -68,8 +104,8 @@ void skaityti2List(list<Studentas2>& studentai2, const string& pav) {
         cerr << "neperskaito...:" << e.what() << endl;
     }
 };
-
-void rusiavimas2List(const list<Studentas2>& studentai2, list<Studentas2>& vargsiukai, list<Studentas2>& kietiakai)
+//--------------------------------------------------------------------------------------------------
+void rusiavimas2List(const list<Studentas2>& studentai2, list<Studentas2>& vargsiukai, list<Studentas2>& kietiakai, int skaiciux)
 {
     kietiakai.clear();
     vargsiukai.clear();
@@ -82,48 +118,60 @@ void rusiavimas2List(const list<Studentas2>& studentai2, list<Studentas2>& vargs
             kietiakai.push_back(studentas);
         }
     }
+    if(skaiciux==0){
+        rusiavimasList2(vargsiukai);
+        rusiavimasList2(kietiakai);
+    }
+    else if(skaiciux==1){
+        rusiavimasList(vargsiukai);
+        rusiavimasList(kietiakai);
+    }
+    else if(skaiciux==2){
+        rusiavimasList3(vargsiukai);
+        rusiavimasList3(kietiakai);
+    }
 }
-
+//-----------------------------------------------------------------------------------------------------------------------
 void isvedimasVList(const string& failas_vargsiukai, const list<Studentas2>& vargsiukai)
 {
     saugojimasList(failas_vargsiukai, vargsiukai);
 }
-
+//-----------------------------------------------------------------------------------------------------------------------
 void isvedimasKList(const string& failas_kietiakai, const list<Studentas2>& kietiakai)
 {
     saugojimasList(failas_kietiakai, kietiakai);
 }
-
-void generavimoLList(list<Studentas2>& studentai2, int skaicius, const string& failas) {
+//-----------------------------------------------------------------------------------------------------------------------
+void generavimoLList(list<Studentas2>& studentai2, int skaicius, const string& failas, const string& failas0, int sk) {
     auto start = std::chrono::high_resolution_clock::now();
-    generavimasList(studentai2, skaicius, failas);
+    generavimasList(studentai2, skaicius, failas, failas0, sk);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     cout << skaicius << " irasu failo generavimas uztruko: " << duration.count() << endl;
 }
-
-void skaitymoLList(list<Studentas2>& studentai2, int skaicius, const string& failas) {
+//-----------------------------------------------------------------------------------------------------------------------
+void skaitymoLList(list<Studentas2>& studentai2, const string& failas) {
     auto start = std::chrono::high_resolution_clock::now();
     skaityti2List(studentai2, failas);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 }
-
-void rusiavimoLList(list<Studentas2>& studentai2, int skaicius, list<Studentas2>& vargsiukai, list<Studentas2>& kietiakai) {
+//-----------------------------------------------------------------------------------------------------------------------
+void rusiavimoLList(list<Studentas2>& studentai2, list<Studentas2>& vargsiukai, list<Studentas2>& kietiakai, int skaiciux) {
     auto start = std::chrono::high_resolution_clock::now();
-    rusiavimas2List(studentai2, vargsiukai, kietiakai);
+    rusiavimas2List(studentai2, vargsiukai, kietiakai, skaiciux);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 }
-
-void isvedimoVLList(const string& failas_vargsiukai, int skaicius, const list<Studentas2>& vargsiukai) {
+//-----------------------------------------------------------------------------------------------------------------------
+void isvedimoVLList(const string& failas_vargsiukai, const list<Studentas2>& vargsiukai) {
     auto start = std::chrono::high_resolution_clock::now();
     isvedimasVList(failas_vargsiukai, vargsiukai);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 }
-
-void isvedimoKLList(const string& failas_kietiakai, int skaicius, const list<Studentas2>& kietiakai) {
+//-----------------------------------------------------------------------------------------------------------------------
+void isvedimoKLList(const string& failas_kietiakai, const list<Studentas2>& kietiakai) {
     auto start = std::chrono::high_resolution_clock::now();
     isvedimasKList(failas_kietiakai, kietiakai);
     auto end = std::chrono::high_resolution_clock::now();
